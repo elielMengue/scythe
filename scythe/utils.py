e9297a9f409490bb7def89c7263b24e559726f5e
Eliel MENGUE <156508960+elielMengue@users.noreply.github.com>
2026-01-29 18:33:23 +0100

    Add a function to verify path that must be ignored while scanning

--- scythe/utils.py
+++ scythe/utils.py
@@ -54,3 +54,19 @@ def calculate_directory_size(path: Path, follow_symlinks: bool = False) -> int:
         pass
 
     return total_size
+
+
+def is_ignored_path(path: Path, custom_ignores: Set[str] = None) -> bool :
+    ignored = IGNORED_PATTERNS.copy()
+    if custom_ignores:
+        ignored.update(custom_ignores)
+
+    if path.name in ignored:
+        return True
+
+    for pattern in ignored:
+        if '*' in pattern :
+            pattern_clean = pattern.replace('*', '')
+            if pattern_clean in path.name :
+                return True
+    return False
