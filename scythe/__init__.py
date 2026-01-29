4278a622df4ae7ba4cb67f002d4c31b6af9ac09e
Eliel MENGUE <156508960+elielMengue@users.noreply.github.com>
2026-01-29 14:52:05 +0100

    Start the CLI entry point cly.py and add the logger setup in the logger.py file

---
+++ scythe/__init__.py
@@ -0,0 +1,10 @@
+"""
+Scythe - Command Line Tool for cleaning artifacts and builds
+"""
+
+
+__version__ = "0.1.0"
+__author__ = "Eliel MENGUE"
+__email__ = "mengueeliel712@gmail.com"
+
+from scythe.cli import cli
--- scythe/cli.py
+++ scythe/cli.py
@@ -1,7 +1,48 @@
-import click
+"""
+Command Line Interface - Implemented with Click-Rich
+"""
 
+import click
+from rich.console import Console
+from rich.panel import Panel
+
+from scythe import __version__
+from scythe.logger import setup_logger, get_logger
+
+console = Console()
 
 @click.group()
-def cli():
-    pass
+@click.version_option(version=__version__, prog_name="SCYTHE")
+@click.option(
+    '--verbose', '-v',
+    is_flag=True,
+    help='Activate verbose mode',
+)
+@click.option(
+    '--no-log-file',
+    is_flag=True,
+    help='Deactivate log mode',
+)
+@click.pass_context
+def cli(ctx, verbose, no_log_file):
+    import logging
+    log_level = logging.DEBUG if verbose else logging.INFO
+    logger = setup_logger(level=log_level, log_file=not no_log_file)
 
+    ctx.ensure_object(dict)
+    ctx.obj["logger"] = logger
+    ctx.obj["console"] = console
+
+    if ctx.invoked_subcommand is None:
+       display_header()
+
+def display_header():
+    header = """
+    [bold red] SCYTHE[/bold red]
+    [yellow] Free your dir in few second [/yellow]
+    """
+
+    console.print(Panel(header, border_style="red"))
+
+if "__main__" == __name__:
+    cli()
